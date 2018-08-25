fc427e00dce108cd145ca545c79b1b22becafb24
Sergey Shchegrikovich <shchegrikovich@gmail.com>
2018-08-25 11:25:08 +0300

    Fixed tests

--- build.ps1
+++ build.ps1
@@ -2,7 +2,7 @@ param($publish = $false, $config = "Release")
 
 dotnet build -c $config
 
-dotnet test --no-build -c $config "$PSScriptRoot\GitDiffReader.Tests\GitDiffReader.Tests"
+dotnet test --no-build -c $config "$PSScriptRoot\GitDiffReader.Tests"
 
 if (Test-Path "$PSScriptRoot\output") {
    Remove-Item -Force -Recurse "$PSScriptRoot\output"
